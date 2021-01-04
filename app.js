49544874cef9e5c2723d34d492e7ed6849ffa807
soo5717 <koo5717@naver.com>
2021-01-04 13:42:02 +0900

    test

--- app.js
+++ app.js
@@ -3,9 +3,18 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const { sequelize } = require('./models');
+
+// Sequelize 연결
+sequelize.sync({ alter: true, force: false })
+.then(() => {
+  console.log('DataBase connection success!');
+})
+.catch((err) => {
+  console.error(err);
+})
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
 
 var app = express();
 
@@ -20,7 +29,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
